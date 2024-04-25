5a34a0a9828e631adb081c39a6d35304ecfbef32
Lea Iodice <liodice@deloitte.com>
2024-04-25 16:13:10 -0400

    saving changes to initial files

---
+++ auth.py
@@ -0,0 +1,32 @@
+#import 
+from flask import redirect, render_template, request, session, url_for
+from app import User, app, db
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if request.method == 'POST':
+        username = request.form.get('username')
+        password = request.form.get('password')
+        hashed_password = generate_password_hash(password, method='sha256')
+
+        new_user = User(username=username, password=hashed_password)
+        db.session.add(new_user)
+        db.session.commit()
+        return redirect(url_for('login'))
+
+    return render_template('register.html')
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        username = request.form.get('username')
+        password = request.form.get('password')
+
+        user = User.query.filter_by(username=username).first()
+        if not user or not check_password_hash(user.password, password):
+            return redirect(url_for('login'))
+
+        session['username'] = user.username
+        return redirect(url_for('home'))
+
+    return render_template('login.html')
