0fe8548d09713817fd1ee2bfea58500992cf099c
Lea Iodice <liodice@deloitte.com>
2024-04-26 14:52:52 -0400

    saving changes, added pubchem API, not tested yet

--- routes.py
+++ routes.py
@@ -1,6 +1,8 @@
 from flask import render_template, request, redirect, url_for, session
 from app import app
 from models import User
+import requests
+
 
 @app.route('/')
 def home():
@@ -10,4 +12,6 @@ def home():
 def search():
     username = session.get('username')
     search_query = request.args.get('search')
-    return {'username': username, 'search_query': search_query}
+    response = requests.get(f'https://pubchem.ncbi.nlm.nih.gov/rest/pug/compound/name/{search_query}/JSON')
+    compound_info = response.json() if response.status_code == 200 else None
+    return {'username': username, 'search_query': search_query, 'compound_info': compound_info}
