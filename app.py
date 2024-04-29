96ebd8129622ffb02770a9f9d05e5941e3fce5f9
Lea Iodice <liodice@deloitte.com>
2024-04-29 11:50:54 -0400

    added secret code to .env and added .env to .gitignore

--- app.py
+++ app.py
@@ -1,9 +1,13 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from dotenv import load_dotenv
+import os
 
+load_dotenv()
 app = Flask(__name__)
 
-app.config['SECRET_KEY'] = 'your-secret-key'
+
+app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite3'
 
 db = SQLAlchemy(app)
