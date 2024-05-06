78b5eca129e0cf5975a8b195b05336c7612928db
Lea Iodice <liodice@deloitte.com>
2024-05-06 18:21:55 -0400

    saving

--- gunicorn_config.py
+++ gunicorn_config.py
@@ -4,8 +4,13 @@ from dotenv import load_dotenv
 
 # Load .env file
 dotenv_path = os.path.join(os.path.dirname(__file__), '.env')
+print(f".env path: {dotenv_path}")
 if os.path.exists(dotenv_path):
-    load_dotenv(dotenv_path)
+    result = load_dotenv(dotenv_path)
+    print(f".env loaded: {result}")
+else:
+    print(".env file does not exist")
+
 
 # Gunicorn config
 bind = "0.0.0.0:8000"
